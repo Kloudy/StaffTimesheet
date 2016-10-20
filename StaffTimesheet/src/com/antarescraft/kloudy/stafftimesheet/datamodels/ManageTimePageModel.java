c9751e1bace1d4e01bb792c9b2001674e312d349
BuildTools <unconfigured@null.spigotmc.org>
2016-10-20 15:55:17 -0600

    implented more of staff settings page

--- StaffTimesheet/src/com/antarescraft/kloudy/stafftimesheet/datamodels/ManageTimePageModel.java
+++ StaffTimesheet/src/com/antarescraft/kloudy/stafftimesheet/datamodels/ManageTimePageModel.java
@@ -3,7 +3,6 @@ package com.antarescraft.kloudy.stafftimesheet.datamodels;
 import java.time.Duration;
 import java.util.UUID;
 
-import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 import com.antarescraft.kloudy.hologui.HoloGUIPlugin;
@@ -11,9 +10,9 @@ import com.antarescraft.kloudy.hologui.guicomponents.GUIPage;
 import com.antarescraft.kloudy.hologui.guicomponents.TextBoxComponent;
 import com.antarescraft.kloudy.hologui.guicomponents.ToggleSwitchComponent;
 import com.antarescraft.kloudy.hologui.guicomponents.ValueScrollerComponent;
-import com.antarescraft.kloudy.hologui.handlers.GUIPageLoadHandler;
-import com.antarescraft.kloudy.hologui.playerguicomponents.PlayerGUIPage;
+import com.antarescraft.kloudy.hologui.handlers.ScrollHandler;
 import com.antarescraft.kloudy.hologui.playerguicomponents.PlayerGUIPageModel;
+import com.antarescraft.kloudy.hologui.scrollvalues.AbstractScrollValue;
 import com.antarescraft.kloudy.hologui.scrollvalues.DurationScrollValue;
 import com.antarescraft.kloudy.plugincore.messaging.MessageManager;
 import com.antarescraft.kloudy.plugincore.time.TimeFormat;
@@ -23,10 +22,10 @@ import net.md_5.bungee.api.ChatColor;
 
 public class ManageTimePageModel extends PlayerGUIPageModel
 {
-	private PlayerGUIPage playerGUIPage;
-	
 	private ValueScrollerComponent loggedTimeScroller;
+	private ValueScrollerComponent loggedTimeHMSScroller;
 	private ValueScrollerComponent timeGoalScroller;
+	private ValueScrollerComponent timeGoalHMSScroller;
 	private ToggleSwitchComponent superAdminToggle;
 	private TextBoxComponent clockInPermissionTextBox;
 	
@@ -37,27 +36,65 @@ public class ManageTimePageModel extends PlayerGUIPageModel
 		super(plugin, guiPage, player);
 		
 		this.staffMember = staffMember;
-				
-		guiPage.registerPageLoadHandler(new GUIPageLoadHandler()
+		
+		loggedTimeScroller = (ValueScrollerComponent)guiPage.getComponent("logged-time-scroller");
+		loggedTimeScroller.setValue(player, new DurationScrollValue(staffMember.getLoggedTime(), 
+				TimeFormat.getMinDuration().plusHours(1), TimeFormat.getMinDuration(), staffMember.getTimeGoal()));
+		
+		timeGoalScroller = (ValueScrollerComponent)guiPage.getComponent("time-goal-scroller");
+		timeGoalScroller.setValue(player, new DurationScrollValue(staffMember.getLoggedTime(), 
+				TimeFormat.getMinDuration().plusHours(1), TimeFormat.getMinDuration(), TimeFormat.getMaxDuration()));
+		
+		superAdminToggle = (ToggleSwitchComponent)guiPage.getComponent("super-admin-toggle");
+		superAdminToggle.setPlayerToggleSwitchState(player, staffMember.isSuperAdmin());
+		
+		clockInPermissionTextBox = (TextBoxComponent)guiPage.getComponent("clock-in-permission");
+		clockInPermissionTextBox.setPlayerTextBoxValue(player, staffMember.getClockInPermission());
+		
+		//register hours:minutes:second scroll handlers
+		loggedTimeHMSScroller = (ValueScrollerComponent)guiPage.getComponent("logged-time-hms-scroller");
+		loggedTimeHMSScroller.registerScrollHandler(new ScrollHandler()
 		{
 			@Override
-			public void onPageLoad(PlayerGUIPage loadedPage)
+			public void onScroll(AbstractScrollValue<?, ?> value)
 			{
-				playerGUIPage = loadedPage;
+				DurationScrollValue loggedTimeValue = (DurationScrollValue)loggedTimeScroller.getPlayerScrollValue(player);
 				
-				loggedTimeScroller = (ValueScrollerComponent)guiPage.getComponent("logged-time-scroller");
-				loggedTimeScroller.setValue(player, new DurationScrollValue(staffMember.getLoggedTime(), 
-						TimeFormat.getMinDuration().plusHours(1), TimeFormat.getMinDuration(), staffMember.getTimeGoal()));
+				if(value.toString().equals("Hours"))
+				{
+					loggedTimeValue.setStep(TimeFormat.getMinDuration().plusHours(1));
+				}
+				else if(value.toString().equals("Minutes"))
+				{
+					loggedTimeValue.setStep(TimeFormat.getMinDuration().plusMinutes(1));
+				}
+				else if(value.toString().equals("Seconds"))
+				{
+					loggedTimeValue.setStep(TimeFormat.getMinDuration().plusSeconds(1));
+				}
+			}
+		});
+		
+		timeGoalHMSScroller = (ValueScrollerComponent)guiPage.getComponent("time-goal-hms-scroller");
+		timeGoalHMSScroller.registerScrollHandler(new ScrollHandler()
+		{
+			@Override
+			public void onScroll(AbstractScrollValue<?, ?> value)
+			{
+				DurationScrollValue timeGoalValue = (DurationScrollValue)timeGoalScroller.getPlayerScrollValue(player);
 				
-				timeGoalScroller = (ValueScrollerComponent)guiPage.getComponent("time-goal-scroller");
-				timeGoalScroller.setValue(player, new DurationScrollValue(staffMember.getLoggedTime(), 
-						TimeFormat.getMinDuration().plusHours(1), TimeFormat.getMinDuration(), TimeFormat.getMaxDuration()));
-				
-				superAdminToggle = (ToggleSwitchComponent)guiPage.getComponent("super-admin-toggle");
-				superAdminToggle.setPlayerToggleSwitchState(player, staffMember.isSuperAdmin());
-				
-				clockInPermissionTextBox = (TextBoxComponent)guiPage.getComponent("clock-in-permission");
-				clockInPermissionTextBox.setPlayerTextBoxValue(player, staffMember.getClockInPermission());
+				if(value.toString().equals("Hours"))
+				{
+					timeGoalValue.setStep(TimeFormat.getMinDuration().plusHours(1));
+				}
+				else if(value.toString().equals("Minutes"))
+				{
+					timeGoalValue.setStep(TimeFormat.getMinDuration().plusMinutes(1));
+				}
+				else if(value.toString().equals("Seconds"))
+				{
+					timeGoalValue.setStep(TimeFormat.getMinDuration().plusSeconds(1));
+				}
 			}
 		});
 	}
@@ -82,17 +119,5 @@ public class ManageTimePageModel extends PlayerGUIPageModel
 		staffMember.setSuperAdmin(superAdminToggle.getPlayerToggleSwitchState(player));
 		
 		MessageManager.info(player, ChatColor.GREEN + "Saved staff member settings!");
-	}
-	
-	private Sound parseSound(String soundStr)
-	{
-		Sound sound = null;
-		try
-		{
-			sound = Sound.valueOf(soundStr);
-		}
-		catch(Exception e){}
-		
-		return sound;
 	}
 }
