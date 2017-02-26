f65933bd866c6ec10dfb3832b5a30bedf4c03c71
Kloudy <kloudy@antarescraft.com>
2017-02-26 09:23:56 -0700

    v1.5

--- StaffTimesheet/src/com/antarescraft/kloudy/stafftimesheet/StaffTimesheet.java
+++ StaffTimesheet/src/com/antarescraft/kloudy/stafftimesheet/StaffTimesheet.java
@@ -19,7 +19,7 @@ public class StaffTimesheet extends HoloGUIPlugin
 	{			
 		pluginName = getName();
 		
-		setMinSupportedApiVersion("1.0.9");
+		setMinSupportedApiVersion("1.1.2");
 		checkMinApiVersion();
 
 		getHoloGUIApi().hookHoloGUIPlugin(this);
